08fd15fd520e3eb8ee2d3cf4a7cb6be4b265b5c9
nitintomar713 <147516907+nitintomar713@users.noreply.github.com>
2025-08-02 17:24:50 +0530

    one more

--- Server.js
+++ Server.js
@@ -20,7 +20,7 @@ app.use(helmet());
 
 // CORS setup
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['https://vsfastfood.netlify.app', 'https://vsfastfoodadmin.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
