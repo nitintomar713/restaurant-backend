7d16a943fa9344355f03cbd0520ae11e4bcc54cc
nitintomar713 <147516907+nitintomar713@users.noreply.github.com>
2025-08-02 16:16:52 +0530

    Update suggestions.js

--- Routes/suggestions.js
+++ Routes/suggestions.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Dish = require('../models/Dish');
+const Dish = require('../Models/Dish');
 
 // POST /api/suggestions
 router.post('/', async (req, res) => {
@@ -38,3 +38,4 @@ router.post('/', async (req, res) => {
 });
 
 module.exports = router;
+
